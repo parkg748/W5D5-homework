5e0566da24209b79c6bbd3974de348b10ef2a377
Grace <parkg748@newschool.edu>
2018-09-20 22:08:20 -0700

    Finish W5D5 homework

---
+++ callbacks_and_functions.js
@@ -0,0 +1,56 @@
+window.setTimeout(function () {
+  alert('HAMMER TIME!');
+}, 5000);
+
+function hammerTime(time) {
+  window.setTimeout(function () {
+    alert(`${time} is hammertime!`)
+  }, 0);
+};
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBuscuits () {
+  let first, second;
+
+  reader.question('Would you like some tea?', (res) => {
+    first = res;
+    console.log(`You replied ${res}.`);
+    reader.question('Would you like some buscuits?', (res) => {
+      second = res;
+      console.log(`You replied ${res}.`);
+      const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+      const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+      console.log(`So you ${firstRes} want tea and you ${secondRes} want buscuits.`);
+      reader.close();
+    });
+  });
+}
+
+
+function Cat () {
+  this.name = 'Markov';
+  this.age = 3;
+}
+
+function Dog () {
+  this.name = 'Noodles';
+  this.age = 4;
+}
+
+Dog.prototype.chase = function (cat) {
+  console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`)
+};
+
+const Markov = new Cat ();
+const Noodles = new Dog ();
+
+Noodles.chase(Markov);
+
+Noodles.chase.call(Markov, Noodles);
+Noodles.chase.apply(Markov, [Noodles]);
